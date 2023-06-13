ce5370b0f31afb5ba243eca160fe1c4e89950ac4
Abbas Muhammad <abbasmuhammad@Abbass-MacBook-Pro.local>
2023-06-13 19:39:44 +0400

    configure multiple swagger files

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,16 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    urls: [
+            {
+              name: "Petstore",
+              url: "https://petstore.swagger.io/v2/swagger.json",
+            },
+            {
+              name: "BitOasis",
+              url: "https://api.bitoasis.net/doc/swagger.json",
+            },
+          ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
